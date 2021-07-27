faa4697c7df77ca3f8582a20b82aedba94696373
Livii Iabanzhi <livannnn@yandex.ru>
2021-07-27 11:45:51 +0900

    [fix]: fixed install and main files

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-virtualenv venv
-source venv/bin/activate
+sudo apt install python3.7 python3-venv python3.7-venv
+virtualenv --python=/usr/bin/python3.7 --no-site-packages venv
+. venv/bin/activate
 pip3 install -r requirements.txt
 rm -rf venv/lib/python3.7/site-packages/gym/envs/
 cp -r ./envs venv/lib/python3.7/site-packages/gym/envs/
